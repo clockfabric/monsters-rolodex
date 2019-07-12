a92334f3a69c7c304862c9a7ca16dbe0b2ebb28e
test <test>
2019-07-12 16:48:21 -0400

    Updates

---
+++ precache-manifest.5243228434696f0dc5a75b481b9d3751.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec14e570c78dfaf22cdd8eb9f40b36db",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "39d0e8a6687fc95bfcaa",
+    "url": "/monsters-rolodex/static/css/main.4f06500a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "39d0e8a6687fc95bfcaa",
+    "url": "/monsters-rolodex/static/js/main.af5ffb31.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
